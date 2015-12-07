2eaf0b9d1a81285d93032b370aa45031ee1f66ff
amy <ayyshi@gmail.com>
2015-12-06 23:14:38 -0500

    name change for controllers file

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,73 @@
 'use strict';
 
-// Client user information
-let socket = io();
-let myUser;
-let token;
+angular.module('TheBookApp', [])
+  .controller('SearchController', SearchController)
+  .controller('BookController', BookController)
 
-const secret = "ihdfioahdfoihasd";
+SearchController.$inject = ['$http'];
+BookController.$inject = ['$http'];
 
-function verifyToken(xhr) {
-	if (localStorage.getItem('userToken')) {
-		xhr.setRequestHeader('Authorization',
-					'Bearer ' + localStorage.getItem('userToken'));
-	}
+function SearchController ($http) {
+  let self = this;
+
+  self.all = [];
+  self.getSearch = getSearch;
+  self.getTitleSearch = getTitleSearch;
+  self.getAuthorSearch = getAuthorSearch;
+
+  function getSearch(){
+
+    $http
+      .get('http://localhost:3000/search/term/' + self.searchTerm)
+      .then(function(response){
+        self.all = response.data;
+      });
+  }
+
+  function getTitleSearch(){
+
+    $http
+      .get('http://localhost:3000/search/title/' + self.searchTitle)
+      .then(function(response){
+        self.all = response.data;
+      });
+  }
+
+  function getAuthorSearch(){
+
+    $http
+      .get('http://localhost:3000/search/author/' + self.searchAuthor)
+      .then(function(response){
+        self.all = response.data;
+      });
+  }
+
+}
+
+function BookController ($http) {
+  let self = this;
+
+  // crud functions
+  self.all = [];
+  self.addBook = addBook;
+  // self.newBook = {};
+
+  function addBook(result){
+    console.log('hitting here');
+    console.log(result);
+    let newBook = {
+      book_id: result.id,
+      title: result.volumeInfo.title,
+      authors: result.volumeInfo.authors,
+      img_url: result.volumeInfo.imageLinks.smallThumbnail
+    }
+
+    $http
+      .post('http://localhost:3000/books/', newBook)
+      .then(function(response){
+        console.log('book added');
+      });
+      self.newBook = {};
+  }
+
 }
