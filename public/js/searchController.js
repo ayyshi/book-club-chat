6276b90168be9cc627f2e60b63f56069f0f125a6
amy <ayyshi@gmail.com>
2015-12-06 19:14:06 -0500

    search by title and author added, all angular render

--- public/js/searchController.js
+++ public/js/searchController.js
@@ -11,12 +11,31 @@ function SearchController ($http) {
   // crud functions
   self.all = [];
   self.getSearch = getSearch;
-
-  getSearch();
+  self.getTitleSearch = getTitleSearch;
+  self.getAuthorSearch = getAuthorSearch;
 
   function getSearch(){
+
     $http
-      .get('http://localhost:3000/search/')
+      .get('http://localhost:3000/search/term/' + this.searchTerm)
+      .then(function(response){
+        self.all = response.data;
+      });
+  }
+
+  function getTitleSearch(){
+
+    $http
+      .get('http://localhost:3000/search/title/' + this.searchTitle)
+      .then(function(response){
+        self.all = response.data;
+      });
+  }
+
+  function getAuthorSearch(){
+
+    $http
+      .get('http://localhost:3000/search/author/' + this.searchAuthor)
       .then(function(response){
         self.all = response.data;
       });
