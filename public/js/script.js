8f5660c25803996d96337f66c810843d64f049ab
amy <ayyshi@gmail.com>
2015-12-07 20:16:11 -0500

    added socket chat

--- public/js/script.js
+++ public/js/script.js
@@ -1,73 +1,51 @@
 'use strict';
 
-angular.module('TheBookApp', [])
-  .controller('SearchController', SearchController)
-  .controller('BookController', BookController)
+let socket = io();
+let myUser;
 
-SearchController.$inject = ['$http'];
-BookController.$inject = ['$http'];
+$(function(){
+  // hide current chat from users not logged into the chat
+  $('.current-chat').hide();
 
-function SearchController ($http) {
-  let self = this;
-
-  self.all = [];
-  self.getSearch = getSearch;
-  self.getTitleSearch = getTitleSearch;
-  self.getAuthorSearch = getAuthorSearch;
-
-  function getSearch(){
-
-    $http
-      .get('http://localhost:3000/search/term/' + self.searchTerm)
-      .then(function(response){
-        self.all = response.data;
-      });
-  }
-
-  function getTitleSearch(){
-
-    $http
-      .get('http://localhost:3000/search/title/' + self.searchTitle)
-      .then(function(response){
-        self.all = response.data;
-      });
-  }
-
-  function getAuthorSearch(){
-
-    $http
-      .get('http://localhost:3000/search/author/' + self.searchAuthor)
-      .then(function(response){
-        self.all = response.data;
-      });
-  }
-
-}
-
-function BookController ($http) {
-  let self = this;
-
-  // crud functions
-  self.all = [];
-  self.addBook = addBook;
-  // self.newBook = {};
-
-  function addBook(result){
-    console.log('hitting here');
-    console.log(result);
-    let newBook = {
-      book_id: result.id,
-      title: result.volumeInfo.title,
-      authors: result.volumeInfo.authors,
-      img_url: result.volumeInfo.imageLinks.smallThumbnail
+  $('#login-input').keypress(function(event){
+    if(event.keyCode === 13){
+      var username = $('#login-input').val();
+      myUser = username;
+      // send to server
+      socket.emit('add user', username + ' joined the room');
+      $('.current-chat').show();
+      $('#login-input').val('');
+      $('#loginpage').hide();
     }
+  });
 
-    $http
-      .post('http://localhost:3000/books/', newBook)
-      .then(function(response){
-        console.log('book added');
-      });
-      self.newBook = {};
-  }
+  $('#message').keypress(function(event){
+    if(event.keyCode === 13){
+      var message = $('#message').val();
+      socket.emit('send message', {name: myUser, message: message})
+      $('#message').val('');
+    }
+  })
 
-}
+});
+
+
+
+// SOCKET events
+
+socket.on('user joined', function(users){
+  var usersList = $('#users ul');
+  usersList.empty();
+  users.forEach(function(user){
+    var userElement = $('<li>');
+    userElement.text(user.name);
+    usersList.append(userElement);
+  });
+});
+
+socket.on('send message', function(data){
+  var chatList = $('#messages');
+  var message = $('<li>');
+  message.text(data.name + " : " + data.message);
+  chatList.append(message);
+});
