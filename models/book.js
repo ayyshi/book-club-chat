400f69698a8aaff964f90bc7e4afc7d302f96824
amy <ayyshi@gmail.com>
2015-12-07 20:12:59 -0500

    changed book id to unique to prevent doubles

--- models/book.js
+++ models/book.js
@@ -3,7 +3,8 @@
 const mongoose = require('mongoose');
 
 let BookSchema = new mongoose.Schema({
-  book_id: { type: String },
+  // unique book_id to prevent the same book being saved to the database twice
+  book_id: { type: String, unique: true },
   title:   { type: String },
   authors:  { type: String },
   img_url: { type: String }
